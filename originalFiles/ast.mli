c50418be25c8faaf2d3a617ca2b0758c7575d2b8
stauffec <cstauffer@CSTAUFFER-T410.corp.natpal.com>
2013-07-27 10:46:17 -0400

    Checking in fix

--- originalFiles/ast.mli
+++ originalFiles/ast.mli
@@ -1,6 +1,27 @@
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
+
 type expr =
-Note of string |
-Rest of string |
-Chord of expr * expr |
-Variable of string |
-Assign of string * expr
+    Literal of int
+  | Id of string
+  | Binop of expr * op * expr
+  | Assign of string * expr
+  | Call of string * expr list
+  | Noexpr
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | While of expr * stmt
+
+type func_decl = {
+    fname : string;
+    formals : string list;
+    locals : string list;
+    body : stmt list;
+  }
+
+type program = string list * func_decl list
+
