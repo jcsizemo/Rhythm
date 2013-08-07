94538ba7ae27645d70e636c5d752a7fb8ad23526
Yuankai Huo <huoyuankai@gmail.com>
2013-08-07 01:07:52 -0400

    rym2MIDI_update2

--- rym2MIDI/source_code/src/rym2MIDI.java
+++ rym2MIDI/source_code/src/rym2MIDI.java
@@ -27,12 +27,16 @@ public class rym2MIDI{
 		int timingRes=4, instrument = 1;
 		int channel=0,note=0,tick=0,velocity=90,column=0;
 
+		int max_tick = 100000;
+		int max_track = 100;
+		int max_note = 200;
+
 		Sequence sequence = null;		
 		File outputFile = new File(args[1]);
 
 		//***** Initialize array ******
-		int NoteArr[][][] = new int[100][][];
-		int trackinfo[][] = new int[100][2];
+		int NoteArr[][][] = new int[max_track][][];
+		int trackinfo[][] = new int[max_track][2];
 
 
 		FileInputStream fstream = new FileInputStream(args[0]);
@@ -46,7 +50,7 @@ public class rym2MIDI{
 		int track_num = -1;
 		int total_track_num = 0;
 		int track_assigned = -2;
-		int ArrTemp[][] = new int[10000][2];
+		int ArrTemp[][] = new int[max_tick][2];
 		//Read File Line By Line
 		while ((strLine = br.readLine()) != null)   {
 			// Print the content on the console
@@ -54,7 +58,7 @@ public class rym2MIDI{
 			if (strLine.indexOf("Track")>=0) {
 				track_num = getTrackNum(strLine);
 				LineNum = 0;
-				ArrTemp = new int[10000][2];
+				ArrTemp = new int[max_tick][2];
 			}
 			else if(strLine.equals("")||strLine.equals("EOF")){
 				//System.out.println (strLine);
@@ -86,12 +90,12 @@ public class rym2MIDI{
 			trackinfo[total_track_num][1] = LineNum;
 			total_track_num = total_track_num + 1;
 		}
-		
+
 		int nChannels = total_track_num;
 
 
 
-		
+
 		//***** Initialize Sequencer *****
 		try{
 			sequence = new Sequence(Sequence.PPQ, timingRes);   //initialize sequencer with timingRes
@@ -100,7 +104,7 @@ public class rym2MIDI{
 			System.exit(1);
 		}
 
-		
+
 		//***** Create tracks and notes *****
 		/* Track objects cannot be created by invoking their constructor
 				   directly. Instead, the Sequence object does the job. So we
@@ -115,26 +119,101 @@ public class rym2MIDI{
 			sm.setMessage(ShortMessage.PROGRAM_CHANGE, i, instrument, 0);  //put in instrument[i] in this track
 			track[i].add(new MidiEvent(sm, 0));
 		}
-		
-	
-	for(int ti=0; ti<nChannels;ti++){
-		int tracknum = trackinfo[ti][0];
-		int notelen = trackinfo[ti][1];
-		int OneTrack[][] = NoteArr[tracknum];
-		for(int ni=0; ni<notelen; ni++){
-			tick = OneTrack[ni][0];
-			note = OneTrack[ni][1];
+
+
+		for(int ti=0; ti<nChannels;ti++){
+			int tracknum = trackinfo[ti][0];
+			int notelen = trackinfo[ti][1];
+			int OneTrack[][] = NoteArr[tracknum];
+			int NoteMatrix[][] = new int[max_note][max_tick];
+			int NoteStat[][] = new int[max_note][2];
 			channel = ti;
-			track[channel].add(createNoteOnEvent(note,tick,channel,velocity));
+			for(int ni=0; ni<notelen; ni++){
+				tick = OneTrack[ni][0];
+				note = OneTrack[ni][1];
+
+				NoteStat[note][0] = 1;
+				int nowTicknum = NoteStat[note][1];
+				NoteMatrix[note][nowTicknum] = tick;
+				int newTicknum = nowTicknum+1;			
+				NoteStat[note][1] = newTicknum;		
+			}
+
+			for(int i=0; i<max_note; i++){
+				if(NoteStat[i][0]==1){
+					int note_value = i;
+					int note_len = NoteStat[i][1];
+					int tick_last = -999;
+					int tick_dur = 0;
+					int tick_ons = 0;
+					for(int j=0; j<note_len;j++){	
+						int tick_now = NoteMatrix[note_value][j];
+						if(j==0){
+							tick_ons = tick_now;
+							tick_dur = 1;
+							tick_last = tick_now;
+						}else if(j==note_len-1){
+							if(tick_now==(tick_last+1)){
+								tick_dur = tick_dur + 1;
+							}else{
+								tick_ons = tick_now;
+								tick_dur = 1;
+							}
+							
+							int jump = 0;
+							for(int k=tick_ons;k<=(tick_ons+tick_dur);k=k+jump){
+								int modnum = k%timingRes;
+								jump = timingRes - modnum;
+								if((k+jump)<(tick_ons+tick_dur)){
+									int tick_ons_oneres = k;
+									int tick_dur_oneres = jump;
+									track[channel].add(createNoteOnEvent(note_value,tick_ons_oneres,channel,velocity));
+									track[channel].add(createNoteOffEvent(note_value,tick_ons_oneres+tick_dur_oneres,channel));	
+								}else{
+									int tick_ons_oneres = k;
+									int tick_dur_oneres = tick_ons+tick_dur-k;
+									track[channel].add(createNoteOnEvent(note_value,tick_ons_oneres,channel,velocity));
+									track[channel].add(createNoteOffEvent(note_value,tick_ons_oneres+tick_dur_oneres,channel));									
+								}						
+							}								
+					
+						}
+						else if(tick_now==(tick_last+1)){
+							tick_dur = tick_dur+1;
+							tick_last = tick_now;
+						}else{
+							int jump = 0;
+							for(int k=tick_ons;k<=(tick_ons+tick_dur);k=k+jump){
+								int modnum = k%timingRes;
+								jump = timingRes - modnum;
+								if((k+jump)<(tick_ons+tick_dur)){
+									int tick_ons_oneres = k;
+									int tick_dur_oneres = jump;
+									track[channel].add(createNoteOnEvent(note_value,tick_ons_oneres,channel,velocity));
+									track[channel].add(createNoteOffEvent(note_value,tick_ons_oneres+tick_dur_oneres,channel));	
+								}else{
+									int tick_ons_oneres = k;
+									int tick_dur_oneres = tick_ons+tick_dur-k;
+									track[channel].add(createNoteOnEvent(note_value,tick_ons_oneres,channel,velocity));
+									track[channel].add(createNoteOffEvent(note_value,tick_ons_oneres+tick_dur_oneres,channel));									
+								}						
+							}	
+							tick_ons = tick_now;
+							tick_dur = 1;
+							tick_last = tick_now;
+						}
+
+					}
+				}
+			}
 		}
-	}
-	
-	try{
-		MidiSystem.write(sequence, 1, outputFile);
-	}catch (IOException e){
-		e.printStackTrace();
-		System.exit(1);
-	}
+
+		try{
+			MidiSystem.write(sequence, 1, outputFile);
+		}catch (IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 
